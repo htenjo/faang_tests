bbf5ad32128d592f668cb0229d7660f2278059f8
Hernan Tenjo <zerovirus23@gmail.com>
2021-08-21 01:28:48 -0500

    Solving SumUpToNumber

---
+++ src/main/java/array/SumUpToNumber.java
@@ -0,0 +1,40 @@
+package array;
+
+import java.util.Arrays;
+
+public class SumUpToNumber {
+    public static int[] findSum(int[] arr, int n) {
+        int[] result = new int[2];
+
+        for (int i=0; i < arr.length; i++) {
+            for (int j=i+1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == n) {
+                    result[0] = arr[i];
+                    result[1] = arr[j];
+                    return result;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 21, 3, 14, 5, 60, 7, 6};
+        int value1 = 27;
+
+        int[] arr2 = {9, 4, 7, 2, 6};
+        int value2 = 17;
+
+        int[] arr3 = {1, 21, 3, 14, 5, 60, 7, 6};
+        int value3 = 81;
+
+        int[] arr4 = {5, 11, 4, 6};
+        int value4 = 10;
+
+        System.out.println("::: Result1 = " + (Arrays.compare(findSum(arr1, value1), new int[]{21, 6})));
+        System.out.println("::: Result2 = " + (Arrays.compare(findSum(arr2, value2), new int[]{9, 4, 7, 2, 6})));
+        System.out.println("::: Result3 = " + (Arrays.compare(findSum(arr3, value3), new int[]{21, 60})));
+        System.out.println("::: Result4 = " + (Arrays.compare(findSum(arr4, value4), new int[]{4, 6})));
+    }
+}
