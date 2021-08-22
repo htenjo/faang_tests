cb0fa32e9849a4aa4cb426ef37440462430bb24f
Hernan Tenjo <zerovirus23@gmail.com>
2021-08-22 13:45:19 -0500

    Solving minimum value in array

--- src/main/java/array/MinimumValue.java
+++ src/main/java/array/MinimumValue.java
@@ -1,2 +1,25 @@
-package array;public class MinimumValue {
+package array;
+
+public class MinimumValue {
+    private static int findMinimum(int[] arr) {
+        int minimum = Integer.MAX_VALUE;
+
+        for (int i : arr) {
+            if (i < minimum) {
+                minimum = i;
+            }
+        }
+
+        return minimum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr0 = new int[]{0};
+        int[] arr1 = new int[]{9, 2, 3, 6};
+        int[] arr2 = new int[]{9, -2, 3, 6};
+
+        System.out.println("::: Min Value0 = " + findMinimum(arr0));
+        System.out.println("::: Min Value1 = " + findMinimum(arr1));
+        System.out.println("::: Min Value2 = " + findMinimum(arr2));
+    }
 }
